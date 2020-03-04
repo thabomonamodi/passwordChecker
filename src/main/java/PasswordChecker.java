e9a81948815e8fc55532ed4ac550c1417f4e7cd1
thabomonamodi <thabomonamodi0@gmail.com>
2020-03-04 10:03:02 +0200

    Password checker updated

--- src/main/java/PasswordChecker.java
+++ src/main/java/PasswordChecker.java
@@ -7,91 +7,102 @@ public class PasswordChecker
         boolean check = true;
         try
         {
-        if (password.length()<8)
-        {
-            System.out.println("password length should be at least 8 characters long.");
-            check=false;
-        }
+            if (password.length()<8)
+            {
+                System.out.println("password length should be 8 characters long.");
+                check=false;
+            }
 
-        if (password.isEmpty())
-        {
-            System.out.println("password cannot be empty.");
-            check=false;
-        }
+            if (password.isEmpty())
+            {
+                System.out.println("password cannot be empty.");
+                check=false;
+            }
 
-        if (!password.matches("(.*[a-z].*)"))
-        {
-            System.out.println("password should contain lowercase.");
-            check=false;
-        }
-        if (!password.matches("(.*[A-Z].*)"))
-        {
-            System.out.println("password should contain uppercase.");
-            check=false;
-        }
+            if (!password.matches("(.*[a-z].*)"))
+            {
+                System.out.println("password should contain lowercase.");
+                check=false;
+            }
+            if (!password.matches("(.*[A-Z].*)"))
+            {
+                System.out.println("password should contain uppercase.");
+                check=false;
+            }
 
-        if (!password.matches("(.*[@#$%!].*)"))
-        {
-            System.out.println("password should have at least one special case.");
-            check=false;
-        }
-        if (!password.matches("(.*[0-9].*)"))
-        {
-            System.out.println("password should have at least one digit.");
-            check=false;
-        }
-
+            if (!password.matches("(.*[@#$%!].*)"))
+            {
+                System.out.println("password should have at least one special case.");
+                check=false;
+            }
+            if(!password.matches("(.*[0-9].*)"))
+            {
+                System.out.println("password should have at least one digit.");
+                check=false;
+            }
         }
         catch (Exception e)
         {
-            System.out.println("make sure the above conditions are met.");
+            System.out.println("make sure the above conditions are met."+e.getMessage());
         }
         return check;
     }
-    public static boolean passwordIsOk(String password)
+    public static boolean passwordIsOk(String password)throws Exception
     {
+
         if (password.length() < 8 || !password.matches("(.*[a-z].*)") || password.isEmpty())
         {
-            System.out.println("password should meet at least 3 of the conditions.");
-            return false;
+            throw new Exception("Password should meet at least 3 of the conditions.\n"+
+                    "1. Password length should be 8 characters long.\n"+
+                    "2. Password cannot be empty.\n"+
+                    "3. Password should contain lowercase.");
+            //System.out.println();
+            //return false;
         }
         if (!password.matches("(.*[A-Z].*)")|| !password.matches("(.*[@#$%!].*)") || !password.matches("(.*[0-9].*)"))
         {
-            System.out.println("password should meet at least 3 of the conditions.");
-            return false;
+            throw new Exception("Password should meet at least 3 of the conditions:\n"+
+                    "1. Password should contain uppercase.\n"+
+                    "2. Password should have at least one special case.\n"+
+                    "3. Password should have at least one digit.");
+            //System.out.println();
         }
+
+
+
         return true;
     }
-    public static boolean passwordIsNeverOk(String password)
+    public static boolean passwordIsNeverOk(String password) throws Exception
     {
         if (password.length() < 8)
         {
             if (password.isEmpty())
             {
-                return false;
+                throw new Exception("Password cannot be empty!");
             }
-            return false;
+            throw new Exception("Password must be 8 characters long!");
         }
+
         return true;
     }
 
-    public static void main(String[] args)
+    public static void main(String[] args)throws Exception
     {
         Scanner input = new Scanner(System.in);
-        System.out.print(
-                "1. A password must have at least eight characters.\n" +
-                        "2. A password consists of only letters and digits.\n" +
-                        "3. A password must contain at least one digits \n" +
-                        "Input a password: \n");
-        String s = input.nextLine();
+            System.out.print(
+                    "1. A password must have at least eight characters.\n" +
+                            "2. A password consists of only letters and digits.\n" +
+                            "3. A password must contain at least one digits \n" +
+                            "Input a password: \n");
+           String s = input.nextLine();
 
         if (password_is_valid(s))
         {
-            System.out.println("Password is valid: " + s);
+            System.out.println(s + ": Password is valid!");
         }
         else
         {
-            System.out.println("Not a valid password: " + s);
+            System.out.println(s + ": Not a valid password!");
         }
         if (passwordIsOk(s))
         {
@@ -101,15 +112,8 @@ public class PasswordChecker
         {
             System.out.println("Password is not okay.");
         }
-        if (passwordIsNeverOk(s))
-        {
-            System.out.println("Password is ok");
-        }
-        else
-        {
-            System.out.println("Password is not okay.");
-        }
-
+        password_is_valid(s);
+        passwordIsNeverOk(s);
     }
 }
 
--- src/test/java/PasswordCheckerTest.java
+++ src/test/java/PasswordCheckerTest.java
@@ -4,12 +4,21 @@ import org.junit.Test;
 public class PasswordCheckerTest
 {
     @Test
-    public void testPassword_is_valid()
+    public void testPassword_is_valid()throws Exception
     {
-        Assert.assertEquals(PasswordChecker.password_is_valid("(.*[a-z].*)"),false);
+        /*Assert.assertEquals(PasswordChecker.password_is_valid("(.*[a-z].*)"),false);
         Assert.assertEquals(PasswordChecker.password_is_valid("(.*[A-Z].*)"),false);
         Assert.assertEquals(PasswordChecker.password_is_valid("(.*[0-9].*)"),false);
         Assert.assertEquals(PasswordChecker.password_is_valid("(.*[@#$%!].*)"),false);
         //Assert.assertEquals(PasswordChecker.password_is_valid());
+        Assert.assertEquals(PasswordChecker.passwordIsNeverOk("zobaRoidz@1"),true);
+        Assert.assertEquals(PasswordChecker.passwordIsOk("zobaRoidz@1"),true);
+        Assert.assertEquals(PasswordChecker.password_is_valid("zobaRoidz@1"),true);*/
+        Assert.assertTrue(PasswordChecker.password_is_valid("zobaRoidz@1"));
+        Assert.assertTrue(PasswordChecker.passwordIsNeverOk("zobaRoidz@1"));
+        Assert.assertTrue(PasswordChecker.passwordIsOk("zobaRoidz@1"));
+        /*new AssertionError(PasswordChecker.password_is_valid("zobaRoidz@1"));
+        new AssertionError(PasswordChecker.passwordIsOk("zobaRoidz@1"));
+        new AssertionError(PasswordChecker.passwordIsNeverOk("zobaRoidz@1"));*/
     }
 }
